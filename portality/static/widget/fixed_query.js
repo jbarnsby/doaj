486743d645a4e92272832c93c6d111a398c81dc1
Steven Eardley <steve@eardley.xyz>
2015-12-15 15:03:40 +0000

    local widget WIP

--- portality/static/widget/fixed_query.js
+++ portality/static/widget/fixed_query.js
@@ -2,6 +2,7 @@
 
     // Localize jQuery variable
     var jQuery;
+    var $;
 
     /******** Load jQuery if not present *********/
     if (window.jQuery === undefined || window.jQuery.fn.jquery !== '1.9.1') {
@@ -23,7 +24,8 @@
     } else {
         // The jQuery version on the window is the one we want to use
         jQuery = window.jQuery;
-        main();
+        $ = window.jQuery;
+        loadWidgetScripts(main);
     }
 
     /******** Called once jQuery has loaded ******/
@@ -31,31 +33,58 @@
         // Restore $ and window.jQuery to their previous values and store the
         // new jQuery in our local jQuery variable
         jQuery = window.jQuery.noConflict(true);
+        $ = jQuery;
         // Call our main function
-        main();
-        //loadWidgetScripts(main);
+        //main();
+        loadWidgetScripts(main);
     }
-
-
-/*    function loadWidgetScripts(callback){
+/*
+    function loadWidgetScripts(callback){
         // Load the scripts the widget requires
         jQuery.when(
             jQuery.getScript('http://localhost:5004/static/portality/vendor/facetview2/es.js'),
             jQuery.getScript('http://localhost:5004/static/portality/vendor/facetview2/bootstrap2.facetview.theme.js'),
             jQuery.getScript('http://localhost:5004/static/doaj/js/doaj.facetview.theme.js'),
-            jQuery.getScript('http://localhost:5004/static/portality/vendor/facetview2/jquery.facetview2.js'),
-
-            jQuery.getScript('http://localhost:5004/static/doaj/js/available_facetviews/public.fixedquerywidget.facetview.js'),
-            jQuery.getScript('http://localhost:5004/static/doaj/js/doaj.js'),
-            jQuery.getScript('http://localhost:5004/static/doaj/js/facetview_results_render_callbacks.js')
+            jQuery.getScript('http://localhost:5004/static/portality/vendor/facetview2/jquery.facetview2.js')
         ).done(callback);
+    }
+
+    function loadWidgetRenderScripts(){
+        jQuery.getScript('http://localhost:5004/static/doaj/js/available_facetviews/public.fixedquerywidget.facetview.js');
+        jQuery.getScript('http://localhost:5004/static/doaj/js/doaj.js');
+        jQuery.getScript('http://localhost:5004/static/doaj/js/facetview_results_render_callbacks.js');
     }*/
 
+    function loadScriptAjax(script, callback){
+        $.ajax({
+            url: script,
+            success: function(data){
+                eval(data);
+                callback();
+            },
+            dataType: 'text'
+        });
+    }
+
+    function loadWidgetScripts(callback){
+        loadScriptAjax('http://localhost:5004/static/portality/vendor/facetview2/es.js', function(){
+            loadScriptAjax('http://localhost:5004/static/portality/vendor/facetview2/bootstrap2.facetview.theme.js', function(){
+                loadScriptAjax('http://localhost:5004/static/doaj/js/doaj.facetview.theme.js', function(){
+                    loadScriptAjax('http://localhost:5004/static/portality/vendor/facetview2/jquery.facetview2.js', callback);
+                });
+            });});
+    }
+
+    function loadWidgetRenderScripts(){
+        loadScriptAjax('http://localhost:5004/static/doaj/js/available_facetviews/public.fixedquerywidget.facetview.js');
+        loadScriptAjax('http://localhost:5004/static/doaj/js/doaj.js');
+        loadScriptAjax('http://localhost:5004/static/doaj/js/facetview_results_render_callbacks.js')
+    }
 
     /******** Our main function ********/
     function main() {
         jQuery(document).ready(function($) {
-            $('#doaj-fixed-query-widget').load("http://localhost:5004/static/widget/fixed_query_body.html");
+            $('#doaj-fixed-query-widget').load("http://localhost:5004/static/widget/fixed_query_body.html",loadWidgetRenderScripts);
         });
     }
 })();
